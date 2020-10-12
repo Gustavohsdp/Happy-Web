c4ba585283c272540646fa0d303bd9110e76e169
Gustavo Henrique <gustavohsdp@gmail.com>
2020-10-12 17:30:24 -0300

    added new map dark

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -32,6 +32,7 @@ function OrphanagesMap() {
         style={{ width: "100%", height: "100%" }}
       >
         {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
+        
         <TileLayer 
         url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
