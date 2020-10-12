7cd9cff5d306624b385792210159eb84137b5101
Gustavo Henrique <gustavohsdp@gmail.com>
2020-10-12 11:12:27 -0300

    landing page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
+
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+
+          <div className="location">
+            <strong>Itabira</strong>
+            <span>Minas Gerais</span>
+          </div>
+
+            <a href="" className="enter-app">
+              >
+            </a>
+      </div>
     </div>
   );
 }
