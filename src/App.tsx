38d6a237e5163029bf16d7dc214e882e4fb53081
Gustavo Henrique <gustavohsdp@gmail.com>
2020-10-12 11:58:29 -0300

    OrphanagesMap page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+import React from "react";
+import { isMappedTypeNode } from "typescript";
 
-import './styles/global.css'
-import './styles/pages/landing.css'
+import "./styles/global.css";
 
-import logoImg from './images/logo.svg'
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-          </main>
-
-          <div className="location">
-            <strong>Itabira</strong>
-            <span>Minas Gerais</span>
-          </div>
-
-            <a href="" className="enter-app">
-              <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-            </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
